56cfb500878e327427fb76845ea8bb6d32484c3c
pjw0194 <pjw019472@gmail.com>
2024-05-23 07:43:25 -0500

    change port

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const indexRouter = require("./routes/index");
+require("dotenv").config();
+
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
 
 const app = express();
 
@@ -10,11 +13,13 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", indexRouter);
 
-const mongoURI = `mongodb://localhost:27017/todo-list`;
+const mongoURI = MONGODB_URI_PROD;
 
 mongoose
 	.connect(mongoURI)
 	.then(() => console.log("mongoose connected"))
 	.catch((err) => console.log("db connection failed..", err));
 
-app.listen(5000, () => console.log("server is listening on port 5000"));
+app.listen(process.env.PORT || 5000, () =>
+	console.log("server is listening on port 5000")
+);
